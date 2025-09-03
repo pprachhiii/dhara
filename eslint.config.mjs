219e4ab1c05bfe783725136f58a91eaad1ddc86d
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-09-03 12:02:47 +0530

    fixing the error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+// eslint.config.mjs
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,12 +14,12 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
+      "node_modules",
+      ".next",
+      "out",
+      "build",
       "next-env.d.ts",
-      "src/generated/**",
+      "src/generated",
     ],
     rules: {
       "@typescript-eslint/no-unused-vars": [
